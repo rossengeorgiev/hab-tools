9689fcfe0d0289a786cb9d99dc8659bfed0db4af
Daniel Richman <main@danielrichman.co.uk>
2013-03-27 12:45:42 +0000

    Changes to aprsnear-us.py
    Unfortunately because I use 4 spaces rather than tabs the diff is rather
    large...
    
     - Fixed bug 'vechicle'
     - Reformat time to HHMMSS
     - Send comment field to spacenearus
     - Use logging
     - Add main method
     - Refactor config to separate (yaml) file; requires PyYAML

--- aprsnear-us.py
+++ aprsnear-us.py
@@ -1,85 +1,100 @@
 #!/usr/bin/env python
+
 import sys
+import os
 import urllib
-import urllib2
-import re
-import json
-from getpass import getpass
-from time import sleep
-from datetime import datetime
 import time
+import datetime
+import json
+import yaml
+import logging
 
-#---------------------------------
-# CONFIG
-#---------------------------------
-apikey 		= "" # (optional) API for your aprs.fi account
+class APRSServerFail(Exception):
+    pass
 
-#---------------------------------
-# Program Code
-#---------------------------------
-if(len(sys.argv) < 2):
-	print "Usage: python %s <callsign>" % sys.argv[0]
-	print "\nUploads latest APRS cordinates and altitude to spacenear.us"
-	sys.exit()
+class NoPositionData(Exception):
+    pass
 
-callsign 	= sys.argv[1];
+class APRS(object):
+    def __init__(self, config):
+        self.callsign = config["callsign"]
+        self.sleep = config["sleep"]
+        self.aprs_url = config["aprs"]["url"]
+        self.aprs_apikey = config["aprs"]["apikey"]
+        self.spacenearus_url = config["spacenearus"]["url"]
+        self.spacenearus_password = config["spacenearus"]["password"]
 
-class aprs:
-	def __init__(self, callsign, apikey = ''):
-		self.csign = callsign
-		self.apikey = apikey
-		self.password = getpass("spacenear.us password: ")
+    def get_aprs(self):
+        logging.debug("Polling APRS")
+        url = self.aprs_url.format(callsign=self.callsign,
+                                   apikey=self.aprs_apikey)
+        response = urllib.urlopen(url).read()
+        logging.debug("APRS Response: {0}".format(response))
+        response = json.loads(response)
 
-	def openurl(self, url, post = []):
-		if(len(post) > 0):
-			data = urllib.urlencode(post)
-			request = urllib2.Request(url, data)
-		else:
-			request = urllib2.Request(url)
+        if response['result'] == "fail":
+            raise APRSServerFail
 
-		return urllib2.urlopen(request).read()
+        if response['found'] < 1:
+            raise NoPositionData
 
-	def check(self):
-		url = "http://api.aprs.fi/api/get?name=%s&what=loc&apikey=%s&format=json" % (self.csign, self.apikey)
-		data = json.loads(self.openurl(url))
+        data = response['entries'][-1]
 
-		if data['result'] == "fail":
-			print "[Error] %s" % data['description']
-			return 0
-		elif int(data['found']) < 1:
-			print "[Error] no position data for %s" % self.csign
-			return 0
+        d = datetime.datetime.utcfromtimestamp(int(data["time"]))
+        time_str = d.strftime("%H%M%S")
 
-		data = data['entries'][-1]
+        post_data = {
+            "vehicle":  self.callsign,
+            "time":     time_str,
+            "lat":      data['lat'],
+            "lon":      data['lng'],
+            "alt":      data.get('altitude', 0),
+            "data":     json.dumps({"comment": data['comment']}),
+            "pass":     self.spacenearus_password
+        }
 
-		self.pos = {
-									"vechicle": "%s (APRS)" % self.csign,
-									"time"		: data['time'],
-									"lat"			: data['lat'],
-									"lon"			: data['lng'],
-									"alt"			: data['altitude'] if data.has_key('altitude') else 0,
-#									"comment"	: data['comment'],
-									"pass"		: self.password 
-								}
+        logging.info("Got: {0}".format(post_data))
 
-		return 1
+        return post_data
 
-	def submitPosition(self):
-		url = "http://spacenear.us/tracker/track.php"
-		temp = self.openurl(url, self.pos).split("\n")[1:]
-		print '[Submited] %s' % ' '.join(temp)
+    def post_spacenearus(self, data):
+        data = urllib.urlencode(data)
+        logging.debug("Posting {0}".format(data))
+        u = urllib.urlopen(self.spacenearus_url.format(data=data))
+        response = u.read()
+        logging.info("Submitted")
+        logging.debug("spacenearus responded: {0!r}".format(response))
+
+    def run(self):
+        while True:
+            try:
+                self.post_spacenearus(self.get_aprs())
+            except KeyboardInterrupt:
+                logging.debug("Exiting...")
+            except:
+                logging.exception("Exception; continuing")
+
+            time.sleep(self.sleep)
 
 
-payload = aprs(callsign, apikey)
+DEFAULT_CONFIG = os.path.join(os.path.dirname(__file__),
+                              "aprsnear-us-config.yml")
 
-while 1:
-	try:
-		if payload.check():
-			payload.submitPosition()
-		
-		sleep(1)
-	except KeyboardInterrupt:
-		print "Exiting..."
-		sys.exit()
-	
-	
+def load_config(filename=DEFAULT_CONFIG):
+    with open(filename) as f:
+        return yaml.safe_load(f)
+
+def main():
+    if len(sys.argv) >= 2:
+        config = load_config(sys.argv[1])
+    else:
+        config = load_config()
+
+    log_level = config["logging_level"].upper()
+    logging.basicConfig(level=getattr(logging, log_level),
+                        format="[%(asctime)s] %(levelname)s: %(message)s")
+
+    APRS(config).run()
+
+if __name__ == "__main__":
+    main()
