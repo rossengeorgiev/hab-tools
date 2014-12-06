eee313991d71530fb0d97abe8277e028b5d65735
Rossen Georgiev <zoorty@gmail.com>
2014-12-06 13:19:28 +0000

    added tool for posting sentence to habitat

---
+++ send_tlm_to_habitat.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import sys
+import httplib
+import json
+from base64 import b64encode
+from hashlib import sha256
+from datetime import datetime
+
+if len(sys.argv) < 2:
+    print "Usage: python %s <sentence> [recv callsign]" % sys.argv[0]
+    sys.exit()
+
+sentence = b64encode(sys.argv[1])
+callsign = sys.argv[2] if len(sys.argv) > 2 else "HABTOOLS"
+
+date = datetime.utcnow().isoformat("T") + "Z"
+
+data = {
+    "type": "payload_telemetry",
+    "data": {
+        "_raw": sentence
+        },
+    "receivers": {
+        callsign: {
+            "time_created": date,
+            "time_uploaded": date,
+            },
+        },
+}
+
+c = httplib.HTTPConnection("habitat.habhub.org")
+c.request(
+    "PUT",
+    "/habitat/_design/payload_telemetry/_update/add_listener/%s" % sha256(sentence).hexdigest(),
+    json.dumps(data),  # BODY
+    {"Content-Type": "application/json"}  # HEADERS
+    )
+
+response = c.getresponse()
+
+print response.status, response.reason
+print response.read()
